d1871b410eb79a6a22513c7eae54cc8762a1e91a
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-20 10:45:42 -0300

    Implementação: criptografia e descriptografar

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const arrowIconLeft = document.getElementById("left-arrow");
 btnEncrypt[0].addEventListener("click", (event) => {
   event.preventDefault();
 
-  let firstInput = textToEncrypt[0].value.trim();
+  let firstInput = encrypt(textToEncrypt[0].value);
 
   if (textToEncrypt[0].value === "") {
     textToEncrypt[0].style.border = "5px solid red";
@@ -24,7 +24,7 @@ btnEncrypt[0].addEventListener("click", (event) => {
 btnEncrypt[1].addEventListener("click", (event) => {
   event.preventDefault();
 
-  let secondInput = textToEncrypt[1].value.trim();
+  let secondInput = decrypt(textToEncrypt[1].value);
 
   if (textToEncrypt[1].value === "") {
     textToEncrypt[1].style.border = "3px solid red";
@@ -51,10 +51,35 @@ textToEncrypt[1].addEventListener("input", () => {
 });
 
 textToEncrypt[0].addEventListener("input", () => {
-  if (textToEncrypt[0].value === '' || textToEncrypt[0] === true) {
-    textToEncrypt[0].value = '';
+  if (textToEncrypt[0].value === "" || textToEncrypt[0] === true) {
+    textToEncrypt[0].value = "";
     textAreaImg.classList.remove("img-disable");
     arrowIconRight.style.opacity = "0";
     arrowIconLeft.style.opacity = "0";
-  } 
+  }
 });
+
+//criptografar
+
+function encrypt(word) {
+  let newWord = word.toLowerCase();
+
+  return newWord
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
+}
+
+//descriptografar
+
+function decrypt(word) {
+  let newWord = word.toLowerCase();
+  return newWord
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+}
