2bc7f5253e4b00a7f1e529502064014a88973250
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-23 23:26:37 -0300

    [JS]: Validação, mostrar erros e regex

--- script.js
+++ script.js
@@ -1,50 +1,79 @@
-const textToEncrypt = document.getElementsByTagName("textarea");
 const btnEncrypt = document.getElementsByTagName("button");
+
+const inputTextToEncrypt = document.getElementById("encrypt-input-area");
+const outputTextToDecrypt = document.getElementById("encrypt-output-area");
+
 const textAreaImg = document.getElementById("textarea-img");
+const textToEncrypt = document.getElementsByTagName("textarea");
+
 const arrowIconRight = document.getElementById("right-arrow");
 const arrowIconLeft = document.getElementById("left-arrow");
 
+const errorCrypt = document.querySelector(".error-el-1");
+const errorDecrypt = document.querySelector(".error-el-2");
+
+/////////////////////////////////////////////////////////////////////////////
+
+//validar
+
+const isValid = function (valueToCheck) {
+  const pattern = /^[a-z]+$/g;
+  const currentValue = valueToCheck.value;
+  const valid = pattern.test(currentValue);
+
+  return valid;
+};
+
+//validar palavra já criptografada
+
+const isCrypt = function (valueToCheck) {
+  const regex = /enter|imes|ai|ober|ufat/g;
+  const result = valueToCheck.value.match(regex);
+
+  return result;
+};
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//////eventos de botões
+
 btnEncrypt[0].addEventListener("click", (event) => {
   event.preventDefault();
+  const validation = isValid(textToEncrypt[0]);
+  const result = isCrypt(textToEncrypt[0]);
 
-  let firstInput = encrypt(textToEncrypt[0].value);
-
-  if (textToEncrypt[0].value === "") {
-    textToEncrypt[0].style.border = "5px solid red";
-  } else {
-    textToEncrypt[1].value = firstInput;
+  if (validation && !result) {
+    textToEncrypt[1].value = encrypt(textToEncrypt[0].value);
     textToEncrypt[0].value = "";
-    textAreaImg.classList.add("img-disable");
-    arrowIconRight.style.stroke = "#34C06B";
-    arrowIconRight.style.transition = "all 0.5s";
     arrowIconRight.style.opacity = "1";
+    textAreaImg.classList.add("img-disable");
+    errorCrypt.setAttribute("hidden", "");
+  } else {
+    errorCrypt.removeAttribute("hidden");
   }
 });
 
 btnEncrypt[1].addEventListener("click", (event) => {
   event.preventDefault();
-
-  let secondInput = decrypt(textToEncrypt[1].value);
-
-  if (textToEncrypt[1].value === "") {
-    textToEncrypt[1].style.border = "3px solid red";
-  } else {
-    textToEncrypt[0].value = secondInput;
+  const validation = isValid(textToEncrypt[1]);
+  const result = isCrypt(textToEncrypt[1]);
+  if (validation && result) {
+    textToEncrypt[0].value = decrypt(textToEncrypt[1].value);
     textToEncrypt[1].value = "";
-    textAreaImg.classList.remove("img-disable");
-    arrowIconLeft.style.stroke = "#34C06B";
-    arrowIconLeft.style.transition = "all 0.5s";
     arrowIconLeft.style.opacity = "1";
+    errorDecrypt.setAttribute("hidden", "");
+  } else {
+    errorDecrypt.removeAttribute("hidden");
   }
 });
 
-////////////////Textarea Events|||||||||||||||||||||||||||
-
+//////eventos de input
 textToEncrypt[1].addEventListener("input", () => {
   if (textToEncrypt[1].value === "") {
     textAreaImg.classList.remove("img-disable");
     arrowIconRight.style.opacity = "0";
     arrowIconLeft.style.opacity = "0";
+    errorDecrypt.setAttribute("hidden", "");
   } else {
     textAreaImg.classList.add("img-disable");
   }
@@ -56,6 +85,8 @@ textToEncrypt[0].addEventListener("input", () => {
     textAreaImg.classList.remove("img-disable");
     arrowIconRight.style.opacity = "0";
     arrowIconLeft.style.opacity = "0";
+    errorCrypt.removeAttribute("hidden");
+    emptyError.removeAttribute("hidden");
   }
 });
 
@@ -85,6 +116,7 @@ function decrypt(word) {
 }
 
 //menu hamburger
+
 const hamburger = document.getElementById("hamburger");
 const navUl = document.getElementById("nav-ul");
 
